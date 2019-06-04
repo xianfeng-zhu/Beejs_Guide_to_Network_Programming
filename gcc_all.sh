566d244b1e397562575a9cde0e3371efb7f1b105
zhuxianfeng13 <zhuxianfeng13@gmail.com>
2019-06-04 23:46:43 +0800

    gcc_all.sh

--- gcc_all.sh
+++ gcc_all.sh
@@ -3,7 +3,7 @@ echo "gcc compile all:"
 echo ""
 
 echo "==== non_blocking_io/selectserver.c:"
-gcc non_blocking_io/selectserver.c -o non_blocking_io/selectserver
+gcc non_blocking_io/selectserver.c -o non_blocking_io/selectserver.out
 code=$?
 echo "$code"
 if [[ $code -ne 0 ]]
@@ -13,7 +13,7 @@ fi
 
 echo ""
 echo "==== simple_stream_sock_demo/client.c:"
-gcc simple_stream_sock_demo/client.c -o simple_stream_sock_demo/client
+gcc simple_stream_sock_demo/client.c -o simple_stream_sock_demo/client.out
 code=$?
 echo "$code"
 if [[ $code -ne 0 ]]
@@ -23,7 +23,7 @@ fi
 
 echo ""
 echo "==== simple_stream_sock_demo/server.c:"
-gcc simple_stream_sock_demo/server.c -o simple_stream_sock_demo/server
+gcc simple_stream_sock_demo/server.c -o simple_stream_sock_demo/server.out
 code=$?
 echo "$code"
 if [[ $code -ne 0 ]]
@@ -33,7 +33,7 @@ fi
 
 echo ""
 echo "==== simple_dgram_sock_demo/listener.c:"
-gcc simple_dgram_sock_demo/listener.c -o simple_dgram_sock_demo/listener
+gcc simple_dgram_sock_demo/listener.c -o simple_dgram_sock_demo/listener.out
 code=$?
 echo "$code"
 if [[ $code -ne 0 ]]
@@ -43,7 +43,7 @@ fi
 
 echo ""
 echo "==== simple_dgram_sock_demo/talker.c:"
-gcc simple_dgram_sock_demo/talker.c -o simple_dgram_sock_demo/talker
+gcc simple_dgram_sock_demo/talker.c -o simple_dgram_sock_demo/talker.out
 code=$?
 echo "$code"
 if [[ $code -ne 0 ]]
